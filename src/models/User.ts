e0157ab7359e2f5f3b156e10e16a294e2a8298f2
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-04-20 21:04:08 -0500

    Add: User model

---
+++ src/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from 'mongoose'
+
+interface IUser {
+  name: string
+  email: string
+  password: string
+}
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email address'],
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: [7, 'Please provide a password greater than 7 characters'],
+  },
+})
+
+const User = mongoose.model<IUser>('User', userSchema)
+
+export default User

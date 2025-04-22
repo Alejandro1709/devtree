37204f59201ac9ebaed7b180ba0f0e7b775eaeef
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-04-21 22:15:04 -0500

    Add: Cors config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import router from './router'
 import 'dotenv/config'
 import { connectDB } from './config/db'
+import { corsConfig } from './config/cors'
 
 const app = express()
 
 connectDB(process.env.MONGO_URI)
 
 app.use(express.json())
+app.use(cors(corsConfig))
 
 app.use('/auth', router)
 

ee842515286b9797791b0ad217970b4be1b40bbe
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-04-22 14:33:46 -0500

    Add: (frontend) Profile page and linktree page

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import AuthLayout from './layouts/AuthLayout'
+import AppLayout from './layouts/AppLayout'
+import LinkTreePage from './pages/LinkTreePage'
+import ProfilePage from './pages/ProfilePage'
 
 export default function Router() {
   return (
@@ -10,6 +13,11 @@ export default function Router() {
         <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/auth/register" element={<RegisterPage />} />
+        </Route>
+
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index={true} element={<LinkTreePage />} />
+          <Route path="profile" element={<ProfilePage />} />
         </Route>
       </Routes>
     </BrowserRouter>

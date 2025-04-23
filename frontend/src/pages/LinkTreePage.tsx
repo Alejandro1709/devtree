43b08e6d430bf826462c56e585b9ef988d01db14
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-04-23 18:00:44 -0500

    Add: DevtreeInput component

--- frontend/src/pages/LinkTreePage.tsx
+++ frontend/src/pages/LinkTreePage.tsx
@@ -1,3 +1,46 @@
+import { useState } from 'react'
+import { social } from '../data/social'
+import DevTreeInput from '../components/DevTreeInput'
+import { isValidUrl } from '../utils'
+import { toast } from 'sonner'
+
 export default function LinkTreePage() {
-  return <div>LinkTreePage</div>
+  const [devTreeLinks, setDevTreeLinks] = useState(social)
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link
+    )
+
+    setDevTreeLinks(updatedLinks)
+  }
+
+  const handleToggleLink = (socialNetwork: string) => {
+    const updatedLinks = devTreeLinks.map((link) => {
+      if (link.name === socialNetwork) {
+        if (isValidUrl(link.url)) {
+          return { ...link, enabled: !link.enabled }
+        } else {
+          toast.error('Invalid URL')
+        }
+      }
+
+      return link
+    })
+
+    setDevTreeLinks(updatedLinks)
+  }
+
+  return (
+    <div className="space-y-5">
+      {devTreeLinks.map((item) => (
+        <DevTreeInput
+          key={item.name}
+          item={item}
+          onUrlChange={handleUrlChange}
+          onLinkChange={handleToggleLink}
+        />
+      ))}
+    </div>
+  )
 }
